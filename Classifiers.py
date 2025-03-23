4c2b02a6a22f369f67324ace231086190f04e035
Alkhawaja <40389133+Alkhawaja@users.noreply.github.com>
2025-03-23 12:17:18 -0400

    Update Classifiers.py

--- Classifiers.py
+++ Classifiers.py
@@ -63,7 +63,7 @@ def extract_bovw_features(directory,num_clusters=50):
     # Step 2: Apply KMeans clustering to form visual words
     kmeans = KMeans(n_clusters=num_clusters, random_state=42, n_init=10)
     kmeans.fit(descriptors_stack)
-    sio.dump(kmeans, "kmeans.skops")
+    sio.dump(kmeans, "Models/kmeans.skops")
     # Step 3: Create histograms for each image
     bovw_features = []
 
@@ -105,10 +105,10 @@ pipeline = Pipeline([
 
 # Define hyperparameter grid
 param_grid = {
-   'pca__n_components': [150, 160],
-    'dt__max_depth': [6, 8],
-    'dt__min_samples_split': [4, 6],
-    'dt__min_samples_leaf': [1, 2],
+   'pca__n_components': [150],
+    'dt__max_depth': [6],
+    'dt__min_samples_split': [4],
+    'dt__min_samples_leaf': [1],
 }
 
 
@@ -127,9 +127,9 @@ dt_hsv = grid_search.best_estimator_
 
 #Define hyperparameter grid SIFT
 param_grid = {
-    'dt__max_depth': [6,  8],
-    'dt__min_samples_split': [4, 6],
-    'dt__min_samples_leaf': [1, 2],
+    'dt__max_depth': [6],
+    'dt__min_samples_split': [4],
+    'dt__min_samples_leaf': [1],
 }
 grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
 
@@ -172,166 +172,166 @@ print("Decision Tree F1 Score:", f1_score(y_test_sift, y_pred_dt, average='weigh
 ConfusionMatrixDisplay(confusion_matrix(y_test_sift, y_pred_dt),display_labels=dt_sift.classes_).plot()
 print(classification_report(y_test_sift, y_pred_dt))
 
-sio.dump(dt_rgb, "dt_rgb.skops")
-sio.dump(dt_hsv, "dt_hsv.skops")
-sio.dump(dt_sift, "dt_sift.skops")
+sio.dump(dt_rgb, "Models/dt_rgb.skops")
+sio.dump(dt_hsv, "Models/dt_hsv.skops")
+sio.dump(dt_sift, "Models/dt_sift.skops")
 
-# %%
-'''
-## Random Forest Classifier
-'''
+# # %%
+# '''
+# ## Random Forest Classifier
+# '''
 
-# %%
-pipeline = Pipeline([
-    ('scaler', StandardScaler()),   # Standardize features
-    ('pca', PCA()),   # PCA without specifying components initially
-    ('rf', RandomForestClassifier(random_state=42))  # Random Forest
-])
+# # %%
+# pipeline = Pipeline([
+#     ('scaler', StandardScaler()),   # Standardize features
+#     ('pca', PCA()),   # PCA without specifying components initially
+#     ('rf', RandomForestClassifier(random_state=42))  # Random Forest
+# ])
 
-# Define hyperparameter grid
-param_grid = {
-    'pca__n_components': [150, 160],  # Tune PCA components
-    'rf__n_estimators': [50, 100],  # Number of trees
-    'rf__max_depth': [None, 10]  # Maximum depth of trees
-}
+# # Define hyperparameter grid
+# param_grid = {
+#     'pca__n_components': [150, 160],  # Tune PCA components
+#     'rf__n_estimators': [50, 100],  # Number of trees
+#     'rf__max_depth': [None, 10]  # Maximum depth of trees
+# }
 
-# Perform Grid Search
-grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
+# # Perform Grid Search
+# grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
 
-grid_search.fit(X_train, y_train)
-rf = grid_search.best_estimator_
+# grid_search.fit(X_train, y_train)
+# rf = grid_search.best_estimator_
 
-grid_search.fit(X_train_rgb, y_train_rgb)
-rf_rgb = grid_search.best_estimator_
+# grid_search.fit(X_train_rgb, y_train_rgb)
+# rf_rgb = grid_search.best_estimator_
 
-grid_search.fit(X_train_hsv, y_train_rgb)
-rf_hsv = grid_search.best_estimator_
+# grid_search.fit(X_train_hsv, y_train_rgb)
+# rf_hsv = grid_search.best_estimator_
 
-#Define hyperparameter grid SIFT
-param_grid = {
-  'rf__n_estimators': [50, 100],  # Number of trees
-    'rf__max_depth': [None, 10]  # Maximum depth of trees
-}
-grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
+# #Define hyperparameter grid SIFT
+# param_grid = {
+#   'rf__n_estimators': [50, 100],  # Number of trees
+#     'rf__max_depth': [None, 10]  # Maximum depth of trees
+# }
+# grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
 
-grid_search.fit(X_train_sift, y_train_sift)
-rf_sift = grid_search.best_estimator_
+# grid_search.fit(X_train_sift, y_train_sift)
+# rf_sift = grid_search.best_estimator_
 
-# %%
-y_pred_rf = rf.predict(X_test)
-print("Random Forest Accuracy:", accuracy_score(y_test, y_pred_rf))
-print("Random Forest Precision:", precision_score(y_test, y_pred_rf, average='weighted'))
-print("Random Forest Recall:", recall_score(y_test, y_pred_rf, average='weighted'))
-print("Random Forest F1 Score:", f1_score(y_test, y_pred_rf, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix(y_test, y_pred_rf),display_labels=rf.classes_).plot()
-print(classification_report(y_test, y_pred_rf))
+# # %%
+# y_pred_rf = rf.predict(X_test)
+# print("Random Forest Accuracy:", accuracy_score(y_test, y_pred_rf))
+# print("Random Forest Precision:", precision_score(y_test, y_pred_rf, average='weighted'))
+# print("Random Forest Recall:", recall_score(y_test, y_pred_rf, average='weighted'))
+# print("Random Forest F1 Score:", f1_score(y_test, y_pred_rf, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix(y_test, y_pred_rf),display_labels=rf.classes_).plot()
+# print(classification_report(y_test, y_pred_rf))
 
-# %%
-y_pred_rf_rgb = rf_rgb.predict(X_test_rgb)
-print("Random Forest Accuracy:", accuracy_score(y_test_rgb, y_pred_rf_rgb))
-print("Random Forest Precision:", precision_score(y_test_rgb, y_pred_rf_rgb, average='weighted'))
-print("Random Forest Recall:", recall_score(y_test_rgb, y_pred_rf_rgb, average='weighted'))
-print("Random Forest F1 Score:", f1_score(y_test_rgb, y_pred_rf_rgb, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix(y_test_rgb, y_pred_rf_rgb),display_labels=rf_rgb.classes_).plot()
-print(classification_report(y_test_rgb, y_pred_rf_rgb))
+# # %%
+# y_pred_rf_rgb = rf_rgb.predict(X_test_rgb)
+# print("Random Forest Accuracy:", accuracy_score(y_test_rgb, y_pred_rf_rgb))
+# print("Random Forest Precision:", precision_score(y_test_rgb, y_pred_rf_rgb, average='weighted'))
+# print("Random Forest Recall:", recall_score(y_test_rgb, y_pred_rf_rgb, average='weighted'))
+# print("Random Forest F1 Score:", f1_score(y_test_rgb, y_pred_rf_rgb, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix(y_test_rgb, y_pred_rf_rgb),display_labels=rf_rgb.classes_).plot()
+# print(classification_report(y_test_rgb, y_pred_rf_rgb))
 
-# %%
-y_pred_rf_hsv = rf_hsv.predict(X_test_hsv)
-print("Random Forest Accuracy:", accuracy_score(y_test_rgb, y_pred_rf_hsv))
-print("Random Forest Precision:", precision_score(y_test_rgb, y_pred_rf_hsv, average='weighted'))
-print("Random Forest Recall:", recall_score(y_test_rgb, y_pred_rf_hsv, average='weighted'))
-print("Random Forest F1 Score:", f1_score(y_test_rgb, y_pred_rf_hsv, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix(y_test_rgb, y_pred_rf_hsv),display_labels=rf_hsv.classes_).plot()
-print(classification_report(y_test_rgb, y_pred_rf_hsv))
+# # %%
+# y_pred_rf_hsv = rf_hsv.predict(X_test_hsv)
+# print("Random Forest Accuracy:", accuracy_score(y_test_rgb, y_pred_rf_hsv))
+# print("Random Forest Precision:", precision_score(y_test_rgb, y_pred_rf_hsv, average='weighted'))
+# print("Random Forest Recall:", recall_score(y_test_rgb, y_pred_rf_hsv, average='weighted'))
+# print("Random Forest F1 Score:", f1_score(y_test_rgb, y_pred_rf_hsv, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix(y_test_rgb, y_pred_rf_hsv),display_labels=rf_hsv.classes_).plot()
+# print(classification_report(y_test_rgb, y_pred_rf_hsv))
 
-# %%
-y_pred_rf = rf_sift.predict(X_test_sift)
-print("Random Forest Accuracy:", accuracy_score(y_test_sift, y_pred_rf))
-print("Random Forest Precision:", precision_score(y_test_sift, y_pred_rf, average='weighted'))
-print("Random Forest Recall:", recall_score(y_test_sift, y_pred_rf, average='weighted'))
-print("Random Forest F1 Score:", f1_score(y_test_sift, y_pred_rf, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix(y_test_sift, y_pred_rf),display_labels=rf_sift.classes_).plot()
-print(classification_report(y_test_sift, y_pred_rf))
-sio.dump(rf_rgb, "rf_rgb.skops")
-sio.dump(rf_hsv, "rf_hsv.skops")
-sio.dump(rf_sift, "rf_sift.skops")
-# %%
-'''
-## Gradient Boosting Classifier
-'''
+# # %%
+# y_pred_rf = rf_sift.predict(X_test_sift)
+# print("Random Forest Accuracy:", accuracy_score(y_test_sift, y_pred_rf))
+# print("Random Forest Precision:", precision_score(y_test_sift, y_pred_rf, average='weighted'))
+# print("Random Forest Recall:", recall_score(y_test_sift, y_pred_rf, average='weighted'))
+# print("Random Forest F1 Score:", f1_score(y_test_sift, y_pred_rf, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix(y_test_sift, y_pred_rf),display_labels=rf_sift.classes_).plot()
+# print(classification_report(y_test_sift, y_pred_rf))
+# sio.dump(rf_rgb, "Models/rf_rgb.skops")
+# sio.dump(rf_hsv, "Models/rf_hsv.skops")
+# sio.dump(rf_sift, "Models/rf_sift.skops")
+# # %%
+# '''
+# ## Gradient Boosting Classifier
+# '''
 
-# %%
-pipeline = Pipeline([
-    ('scaler', StandardScaler()),   # Standardize features
-    ('pca', PCA()),   # PCA without specifying components initially
-    ('gb', XGBClassifier(random_state=42))  # XGBoost
-])
+# # %%
+# pipeline = Pipeline([
+#     ('scaler', StandardScaler()),   # Standardize features
+#     ('pca', PCA()),   # PCA without specifying components initially
+#     ('gb', XGBClassifier(random_state=42))  # XGBoost
+# ])
 
-# Define hyperparameter grid
-param_grid = {
-    'pca__n_components': [150, 160],  # Tune PCA components
-    'gb__n_estimators': [50, 100],  # Number of boosting stages
-    'gb__learning_rate': [0.01, 0.1]  # Learning rate
-}
+# # Define hyperparameter grid
+# param_grid = {
+#     'pca__n_components': [150, 160],  # Tune PCA components
+#     'gb__n_estimators': [50, 100],  # Number of boosting stages
+#     'gb__learning_rate': [0.01, 0.1]  # Learning rate
+# }
 
-# Perform Grid Search
-grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
+# # Perform Grid Search
+# grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
 
-grid_search.fit(X_train, y_train)
-gb = grid_search.best_estimator_
+# grid_search.fit(X_train, y_train)
+# gb = grid_search.best_estimator_
 
-grid_search.fit(X_train_rgb, y_train_rgb)
-gb_rgb = grid_search.best_estimator_
+# grid_search.fit(X_train_rgb, y_train_rgb)
+# gb_rgb = grid_search.best_estimator_
 
-grid_search.fit(X_train_hsv, y_train_rgb)
-gb_hsv = grid_search.best_estimator_
+# grid_search.fit(X_train_hsv, y_train_rgb)
+# gb_hsv = grid_search.best_estimator_
 
-#Define hyperparameter grid SIFT
-param_grid = {
-    'gb__n_estimators': [50, 100],  # Number of boosting stages
-    'gb__learning_rate': [0.01, 0.1]  # Learning rate
-}
-grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
+# #Define hyperparameter grid SIFT
+# param_grid = {
+#     'gb__n_estimators': [50, 100],  # Number of boosting stages
+#     'gb__learning_rate': [0.01, 0.1]  # Learning rate
+# }
+# grid_search = GridSearchCV(pipeline, param_grid, cv=2, scoring='accuracy')
 
-grid_search.fit(X_train_sift, y_train_sift)
-gb_sift = grid_search.best_estimator_
+# grid_search.fit(X_train_sift, y_train_sift)
+# gb_sift = grid_search.best_estimator_
 
-# %%
-y_pred_gb = gb.predict(X_test)
-print("Gradient Boosting Accuracy:", accuracy_score(y_test, y_pred_gb))
-print("Gradient Boosting Precision:", precision_score(y_test, y_pred_gb, average='weighted'))
-print("Gradient Boosting Recall:", recall_score(y_test, y_pred_gb, average='weighted'))
-print("Gradient Boosting F1 Score:", f1_score(y_test, y_pred_gb, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred_gb),display_labels=gb.classes_).plot()
-print(classification_report(y_test, y_pred_gb))
+# # %%
+# y_pred_gb = gb.predict(X_test)
+# print("Gradient Boosting Accuracy:", accuracy_score(y_test, y_pred_gb))
+# print("Gradient Boosting Precision:", precision_score(y_test, y_pred_gb, average='weighted'))
+# print("Gradient Boosting Recall:", recall_score(y_test, y_pred_gb, average='weighted'))
+# print("Gradient Boosting F1 Score:", f1_score(y_test, y_pred_gb, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred_gb),display_labels=gb.classes_).plot()
+# print(classification_report(y_test, y_pred_gb))
 
-# %%
-y_pred_gb_rgb = gb_rgb.predict(X_test_rgb)
-print("Gradient Boosting Accuracy:", accuracy_score(y_test_rgb, y_pred_gb_rgb))
-print("Gradient Boosting Precision:", precision_score(y_test_rgb, y_pred_gb_rgb, average='weighted'))
-print("Gradient Boosting Recall:", recall_score(y_test_rgb, y_pred_gb_rgb, average='weighted'))
-print("Gradient Boosting F1 Score:", f1_score(y_test_rgb, y_pred_gb_rgb, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test_rgb, y_pred_gb_rgb),display_labels=gb_rgb.classes_).plot()
-print(classification_report(y_test_rgb, y_pred_gb_rgb))
+# # %%
+# y_pred_gb_rgb = gb_rgb.predict(X_test_rgb)
+# print("Gradient Boosting Accuracy:", accuracy_score(y_test_rgb, y_pred_gb_rgb))
+# print("Gradient Boosting Precision:", precision_score(y_test_rgb, y_pred_gb_rgb, average='weighted'))
+# print("Gradient Boosting Recall:", recall_score(y_test_rgb, y_pred_gb_rgb, average='weighted'))
+# print("Gradient Boosting F1 Score:", f1_score(y_test_rgb, y_pred_gb_rgb, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test_rgb, y_pred_gb_rgb),display_labels=gb_rgb.classes_).plot()
+# print(classification_report(y_test_rgb, y_pred_gb_rgb))
 
-# %%
-y_pred_gb_hsv = gb_hsv.predict(X_test_hsv)
-print("Gradient Boosting Accuracy:", accuracy_score(y_test_rgb, y_pred_gb_hsv))
-print("Gradient Boosting Precision:", precision_score(y_test_rgb, y_pred_gb_hsv, average='weighted'))
-print("Gradient Boosting Recall:", recall_score(y_test_rgb, y_pred_gb_hsv, average='weighted'))
-print("Gradient Boosting F1 Score:", f1_score(y_test_rgb, y_pred_gb_hsv, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test_rgb, y_pred_gb_hsv),display_labels=gb_hsv.classes_).plot()
-print(classification_report(y_test_rgb, y_pred_gb_hsv))
+# # %%
+# y_pred_gb_hsv = gb_hsv.predict(X_test_hsv)
+# print("Gradient Boosting Accuracy:", accuracy_score(y_test_rgb, y_pred_gb_hsv))
+# print("Gradient Boosting Precision:", precision_score(y_test_rgb, y_pred_gb_hsv, average='weighted'))
+# print("Gradient Boosting Recall:", recall_score(y_test_rgb, y_pred_gb_hsv, average='weighted'))
+# print("Gradient Boosting F1 Score:", f1_score(y_test_rgb, y_pred_gb_hsv, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test_rgb, y_pred_gb_hsv),display_labels=gb_hsv.classes_).plot()
+# print(classification_report(y_test_rgb, y_pred_gb_hsv))
 
-# %%
-y_pred_gb = gb_sift.predict(X_test_sift)
-print("Gradient Boosting Accuracy:", accuracy_score(y_test_sift, y_pred_gb))
-print("Gradient Boosting Precision:", precision_score(y_test_sift, y_pred_gb, average='weighted'))
-print("Gradient Boosting Recall:", recall_score(y_test_sift, y_pred_gb, average='weighted'))
-print("Gradient Boosting F1 Score:", f1_score(y_test_sift, y_pred_gb, average='weighted'))
-ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test_sift, y_pred_gb),display_labels=gb_sift.classes_).plot()
-print(classification_report(y_test_sift, y_pred_gb))
+# # %%
+# y_pred_gb = gb_sift.predict(X_test_sift)
+# print("Gradient Boosting Accuracy:", accuracy_score(y_test_sift, y_pred_gb))
+# print("Gradient Boosting Precision:", precision_score(y_test_sift, y_pred_gb, average='weighted'))
+# print("Gradient Boosting Recall:", recall_score(y_test_sift, y_pred_gb, average='weighted'))
+# print("Gradient Boosting F1 Score:", f1_score(y_test_sift, y_pred_gb, average='weighted'))
+# ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test_sift, y_pred_gb),display_labels=gb_sift.classes_).plot()
+# print(classification_report(y_test_sift, y_pred_gb))
 
-sio.dump(gb_rgb, "gb_rgb.skops")
-sio.dump(gb_hsv, "gb_hsv.skops")
-sio.dump(gb_sift, "gb_sift.skops")
+# sio.dump(gb_rgb, "Models/gb_rgb.skops")
+# sio.dump(gb_hsv, "Models/gb_hsv.skops")
+# sio.dump(gb_sift, "Models/gb_sift.skops")
